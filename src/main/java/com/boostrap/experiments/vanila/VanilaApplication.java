f2a10f37f9aca0afda827b7fa80cc0b86e1ea73f
Roshni.Prince <roshni.prince@target.com>
2019-05-31 14:47:26 +0530

    First commit

---
+++ src/main/java/com/boostrap/experiments/vanila/VanilaApplication.java
@@ -0,0 +1,13 @@
+package com.boostrap.experiments.vanila;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VanilaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VanilaApplication.class, args);
+	}
+
+}
